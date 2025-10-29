b96c568efc09a6690ee466b5245bf3057f1edd8d
PEDRO RAFAEL LIMA BEZERRA DE MEDEIROS <pedromedeiros0978@digitalcollege.internal>
2025-10-28 21:13:49 -0300

    agora sim

--- modulo.js
+++ modulo.js
@@ -1,14 +1,26 @@
-const os = require('os');
-const { converterByteToMb } = require('./util/conversor');
-const chalk = require('chalk');
-const tipo = os.type()
-const plataforma = os.platform()
-const memoria_livre = os.freemem()
+import os from 'os';
+import chalk from 'chalk';
+import { converterByteToMb } from './util/conversor.js'; /* importar o modulo costumizado */
 
-console.log(chalk.blue.bold('=== informações do sistema ==='))
+const log = console.log;
 
-console.log(`Tipo: ${tipo}`)
-console.log(`Tipo: ${plataforma}`)
-console.log(`Tipo: ${os.release()}`)
-console.log(`Tipo: ${memoria_livre} bytes`)
-console.log(`Tipo: ${converterByteToMb(memoria_livre)} MB`)
+const tipo = os.type();
+const plataforma = os.platform();
+const memoria_livre = os.freemem();
+const cpus = os.cpus();
+
+log(chalk.blue.bold('=== informações do sistema ==='))
+
+log(`Tipo: ${tipo}`)
+log(`Tipo: ${plataforma}`)
+log(`Tipo: ${os.release()}`)
+log(`Tipo: ${memoria_livre} bytes`)
+log(`Tipo: ${converterByteToMb(memoria_livre)} MB`)
+
+
+// ES2015 template literal
+log(`
+CPU(s): ${cpus.red('90%')}
+RAM: ${chalk.green('40%')}  
+DISK: ${chalk.yellow('70%')}
+`)
--- util/conversor.js
+++ util/conversor.js
@@ -1,6 +1,5 @@
 // Função para converter bytes para MB
-const converterByteToMb = (bytes) => {
+// exportando o modulo (funcao de conversão) para utilizar em qualquer outro arquivo
+export const converterByteToMb = (bytes) => {
     return bytes / (1024 * 1024).toFixed(2);
 };
-
-module.exports = { converterByteToMb };
